b9685a4cc7d744ee354061d75474ceb9053f119f
Jonas Salo <salomannen1234@gmail.com>
2026-08-07 21:20:02 +0200

    infiniti: Enable bypass charging
    Change-Id: I87440ca66bd4f9418ee9a7798db528f6ed283e73

--- infinity_infiniti.mk
+++ infinity_infiniti.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/oneplus/infiniti/device.mk)
 # Inherit some common Lineage stuff.
 INFINITY_MAINTAINER := koaaN
 TARGET_HAS_UDFPS := true
+BYPASS_CHARGE_SUPPORTED := true
 
 $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
